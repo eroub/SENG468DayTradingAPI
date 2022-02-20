b40c5a5ac65a0afe0ebe548d1e5a3156546ebb72
Connor Buchko <connorbuchko2010@yahoo.ca>
2022-02-20 14:44:27 -0800

    finished quote function for generating a price

--- app/controllers/Misc.controller.js
+++ app/controllers/Misc.controller.js
@@ -23,7 +23,7 @@ exports.add = (userid, amount) => {
     //             return false;
     //         });
     //     } else {
-    //         User.update({ funds: userObj.amount + data[0].dataValues.Funds}, {where: {UsrName: userObj.UserName }})
+    //         User.update({ funds: userObj.amount + data[0].dataValues.Funds}, {where: {UserName: userObj.UserName }})
     //     }
     // })
 
@@ -31,6 +31,39 @@ exports.add = (userid, amount) => {
 
 exports.quote = (userid, stock) => {
     // Purpose: Get the current quote for the stock for the specified user
+    const stockSymbol = stock.split(0,3);
+    let stockNumber = 0;
+    stockSymbol.forEach((letter) => {
+        const ascii = letter.charCodeAt(0);
+        stockNumber += ascii;
+    });
+
+    //a stock symbols base price is the sum of its ascii values
+    //add ten to get more interesting numbers at the lower range
+    const basePrice = stockNumber + 10;
+
+    const date = new Date();
+    const time = date.getTime();
+    const timeMod = time % 10;
+
+    //pick a random number between 11 and 20
+    const rand = Math.floor(Math.random() * (20 - 11 + 1)) + 11;
+
+    let spikeMag = rand;
+    //convert from two digit to one (ie. from 11 to 1.1)
+    spikeMag /= Math.pow(10, 1);
+    //spike magnitude is a random number between 1.1 and 2.0
+    
+    let stockPrice = basePrice;
+    //decrease the base price by the spike magnitude if the time ends in 0-4
+    if(timeMod < 5){
+        stockPrice =  basePrice/spikeMag;
+    //increase the base price by the spike magnitude if the time ends in 5-9
+    } else {
+        stockPrice = basePrice*spikeMag;
+    }
+
+    return stockPrice;
 }
 
 exports.dumplog = (filename) => {
@@ -56,7 +89,7 @@ exports.destroyAll = () => {
     User.destroy({
         where: {},
         truncate: true,
-    }).catch(() => errored = 1)
+    }).catch(() => error = 1)
 
     if(!error) return true;
     return false;
